071a2630f7edbbf0197a5d48b5c03c4e4c8995b4
Ben Hickson <bzhickson@gmail.com>
2020-02-09 02:16:17 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -15,7 +15,11 @@ end
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
-  []
+  output = []
+  assembled_aoh.length.times do |index|
+    output.push()
+  end
+  output = assembled_aoh
 end
 
 def aoh_lookup(aoh, row, key)
